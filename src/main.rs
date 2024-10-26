3435ed503d67db3f8cdad62425d9c526a9868759
xosnrdev <successxodev@gmail.com>
2024-10-26 00:41:47 -0400

    Refactor(main): Update server configuration and listener setup

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,32 @@
+use std::net::TcpListener;
+
 use actix_web::{web, App, HttpServer};
-use authnorization::handler::config;
+use authnorization::{configuration::AppState, controllers::config};
 use dotenv::dotenv;
 use env_logger::Env;
-use sqlx::PgPool;
-use std::env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
     env_logger::init_from_env(Env::default().default_filter_or("info"));
 
-    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let db_pool = PgPool::connect(&db_url)
-        .await
-        .expect("Failed to connect to the database");
+    let app_state = AppState::new();
+
+    let address = format!(
+        "{}:{}",
+        app_state.get_app_config().get_server().get_host(),
+        app_state.get_app_config().get_server().get_port()
+    );
+    let workers = app_state.get_app_config().get_server().get_workers();
+    let listener = TcpListener::bind(address)?;
 
     HttpServer::new(move || {
         App::new()
-            .app_data(web::Data::new(db_pool.clone()))
+            .app_data(web::Data::new(app_state.clone()))
             .configure(config)
     })
-    .bind(("127.0.0.1", 8080))?
+    .workers(workers)
+    .listen(listener)?
     .run()
     .await
 }
