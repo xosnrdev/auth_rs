7885870e5bdf284314bcd80b10dbaef7875cb6d0
xosnrdev <successxodev@gmail.com>
2024-10-26 00:40:22 -0400

    Refactor(dto): Add email deserialization utility function

--- src/dto/mod.rs
+++ src/dto/mod.rs
@@ -5,3 +5,16 @@ mod user;
 pub use auth::*;
 pub use token::*;
 pub use user::*;
+use utils::*;
+
+mod utils {
+    use serde::{Deserialize, Deserializer};
+
+    pub fn deserialize_email<'de, D>(deserializer: D) -> Result<String, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let email = String::deserialize(deserializer)?;
+        Ok(email.to_lowercase())
+    }
+}
