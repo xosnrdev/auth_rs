e8853d3066e2379ae275a339137cbb6e8ecda6c4
xosnrdev <successxodev@gmail.com>
2024-10-26 00:40:54 -0400

    Refactor(db): Add columns to users table and create refresh_tokens table

--- src/db/migrations/20241010101031_auth.sql
+++ src/db/migrations/20241010101031_auth.sql
@@ -2,7 +2,22 @@
 CREATE TABLE users (
     id UUID PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
-    password_hash TEXT NOT NULL
+    password_hash TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL ,
+    updated_at TIMESTAMPTZ NOT NULL
 );
 
 CREATE INDEX idx_users_email ON users(email);
+
+CREATE TABLE refresh_tokens (
+    id UUID PRIMARY KEY,
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    token TEXT NOT NULL,
+    expires_at TIMESTAMPTZ NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL,
+    updated_at TIMESTAMPTZ NOT NULL,
+    revoked BOOLEAN NOT NULL
+);
+
+CREATE INDEX idx_refresh_tokens_user_id ON refresh_tokens(user_id);
+CREATE INDEX idx_refresh_tokens_token ON refresh_tokens(token);
