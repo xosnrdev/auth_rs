c1f2a0d7be7708742e3ebe9f1b52a23d23203506
xosnrdev <successxodev@gmail.com>
2024-11-01 05:52:53 -0400

    Refactor(main): Add rate limiting middleware and configuration

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use std::net::TcpListener;
-
 use actix_web::{web, App, HttpServer};
-use auth_rs::{configuration::AppState, controllers::config};
+use auth_rs::{configuration::AppState, controllers::config, middlewares::RateLimitMiddleware};
 use dotenv::dotenv;
 use env_logger::Env;
+use std::net::TcpListener;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -20,8 +19,14 @@ async fn main() -> std::io::Result<()> {
     let workers = app_state.get_app_config().get_server().get_workers();
     let listener = TcpListener::bind(address)?;
 
+    let rate_limit_config = app_state.get_app_config().get_rate_limit().clone();
+
     HttpServer::new(move || {
+        let (rate_limiter, limiter) = RateLimitMiddleware::new(&rate_limit_config).unwrap();
+
         App::new()
+            .wrap(rate_limiter)
+            .app_data(limiter)
             .app_data(web::Data::new(app_state.clone()))
             .configure(config)
     })
